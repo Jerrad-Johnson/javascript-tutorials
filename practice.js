0c96dde7ef316134bbc09a38a7f05b501b01faa6
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-02 15:46:45 -0600

    Basic setup, and tested pass-by-reference.

---
+++ practice.js
@@ -0,0 +1,16 @@
+let cc = console.log;
+
+var cd = (val) => {
+    let elem = document.querySelector("#jshere");
+    elem.innerHTML = val;
+}
+
+function pbr(){
+    let x = 5;
+    let y = x;
+    y = 2;
+    cd(x);
+}
+
+//pbr();
+
